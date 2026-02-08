8220342ffbcaf0184bea89f7ec82471d60bbc4d4
luho91 <lukas.holzheid@mail.de>
2026-02-08 07:39:48 +0100

    Finish site

--- src/generate.py
+++ src/generate.py
@@ -19,3 +19,32 @@ def generate_page(from_path, template_path, dest_path):
     dst = Path(dest_path)
     dst.parent.mkdir(parents=True, exist_ok=True)
     dst.write_text(new_content, encoding="utf-8")
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    root = Path(__file__).resolve().parents[1]
+    template_path = (root / template_path).resolve()
+
+    src = Path(dir_path_content)
+    if not src.is_absolute():
+        src = root / src
+    src = src.resolve()
+
+    dst = Path(dest_dir_path)
+    if not dst.is_absolute():
+        dst = root / dst
+    dst = dst.resolve()
+
+    if not dst.exists():
+        dst.mkdir(parents=True, exist_ok=True)
+
+    content = list(src.iterdir())
+
+    for el in content:
+        if el.is_file() and el.suffix == ".md":
+            rel = el.relative_to(src)
+            new_name = (dst / rel).with_suffix(".html")
+            generate_page(el, template_path, new_name)
+        if el.is_dir():
+            new_dst = dst / el.name
+            generate_pages_recursive(el, template_path, new_dst)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from copy import copy_all_from_source_to_target
-from generate import generate_page
+from generate import generate_pages_recursive
 
 
 def main():
     copy_all_from_source_to_target("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 
 if __name__ == "__main__":
